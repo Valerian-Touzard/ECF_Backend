725ee0b0ac4770f83df968133dfe8666e4612ad1
Valerian Touzard <vtouzard244@gmail.com>
2022-12-08 09:00:46 +0100

    Initialisation du repo

---
+++ src/main/java/fr/valerian/formation/ecf/ecf_backend/EcfBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.valerian.formation.ecf.ecf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/valerian/formation/ecf/ecf_backend/EcfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.valerian.formation.ecf.ecf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
